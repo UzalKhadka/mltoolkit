845a1ee1114582ad545d819dd11f8dda9dbad560
Ujjal Khadka <khadkauzal@gmail.com>
2022-10-15 18:32:29 +0545

    added-r2_score-function

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,6 +1,8 @@
 from ._encodings import LabelEncoder
 from ._encodings import OneHotEncoder
 from ._metrics import mean_squared_error
+from ._metrics import mean_absolute_error
+from ._metrics import r2_score
 from ._preprocessings import train_test_split
 
 __all__ = [
@@ -8,5 +10,6 @@ __all__ = [
     "OneHotEncoder",
     "mean_squared_error",
     "mean_absolute_error",
+    "r2_score",
     "train_test_split",
 ]
--- utilities/_metrics.py
+++ utilities/_metrics.py
@@ -19,3 +19,20 @@ def mean_absolute_error(y_true, y_pred):
     y_pred = np.array(y_pred)
 
     return np.mean(np.abs(y_true - y_pred))
+
+
+# R2 Score function
+def r2_score(y_true, y_pred):
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    # calculating the residuals and total sum of squares
+    SSres = np.sum((y_true - y_pred) ** 2)
+    SStot = np.sum((y_true - np.mean(y_true)) ** 2)
+
+    # applying the formula
+    r2_score = 1 - np.divide(
+        SSres, SStot, out=np.zeros_like(SSres, dtype=type(SStot)), where=SStot != 0
+    )  # using np.divide to avoid ZeroDivisionError
+
+    return r2_score
