bfea1c1de702e3673014d9b2ca501fceadb111bd
Ujjal Khadka <khadkauzal@gmail.com>
2022-10-17 22:10:48 +0545

    added-confusion-matrix-function

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -3,6 +3,7 @@ from ._encodings import OneHotEncoder
 from ._metrics import mean_squared_error
 from ._metrics import mean_absolute_error
 from ._metrics import r2_score
+from ._metrics import confusion_matrix
 from ._preprocessings import train_test_split
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "mean_absolute_error",
     "r2_score",
     "train_test_split",
+    "confusion_matrix",
 ]
--- utilities/_metrics.py
+++ utilities/_metrics.py
@@ -36,3 +36,35 @@ def r2_score(y_true, y_pred):
     )  # using np.divide to avoid ZeroDivisionError
 
     return r2_score
+
+
+# Confusion Matrix Function
+def confusion_matrix(y_true, y_pred, labels=None):
+    assert len(y_true) == len(y_pred), "Length of y_true and y_pred must be equal"
+
+    if labels is None or len(labels) == 0:
+        labels = np.unique(
+            y_true + y_pred
+        )  # get all unique labels from both y_true and y_pred
+    else:
+        labels = np.unique(labels)
+
+    len_labels = len(labels)  # number of unique labels
+    # create a confusion matrix
+    conf_matrix = np.zeros((len_labels, len_labels), dtype=int)
+
+    # fill the confusion matrix
+    for i in range(len(y_true)):  # for each y_true sample
+        # iterating over all labels to find the index of y_true sample in labels
+        for j in range(len_labels):
+            if y_true[i] == labels[j]:  # check for match
+                # iterating over all labels to find the index of y_pred sample in labels
+                for k in range(len_labels):
+                    # check for match and now for y_pred sample
+                    if y_pred[i] == labels[k]:
+                        # increment the value of confusion matrix at the index of y_true and y_pred sample
+                        conf_matrix[j][k] += 1
+                        break  # for optimization
+                break  # for optimization
+
+    return conf_matrix
